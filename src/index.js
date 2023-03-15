0c02b51514c9cd0f02ebc105569645950feb644c
Ryan Jolley <ryan.jolley0@gmail.com>
2023-03-14 22:14:54 -0600

    changed to hash router again

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter} from "react-router-dom";
+import { HashRouter} from "react-router-dom";
 import './index.css'
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 );
