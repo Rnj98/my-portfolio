604cb64f8dbd1de20350303576719557136295f2
Ryan Jolley <ryan.jolley0@gmail.com>
2023-03-15 12:20:29 -0600

    added resizing for phone screen

--- src/components/pages/Contact/Contact.jsx
+++ src/components/pages/Contact/Contact.jsx
@@ -5,16 +5,21 @@ import "./Contact.css"
 export default function Contact() {
   return (
     <div className='mainContact'>
-      <div className='divGreeter'>I would love to hear from you!
-      <br>
-      </br>
-      If you have any questions, or would like a copy of my resume, please send me a message here!</div>
-      <br></br>
-      <div className='contactForm'>
+      <div className='divGreeter'>
+        <p>
+        I would love to hear from you!
+        If you have any questions, or would like a copy of my resume, please send me a message here!
+        </p>
+        </div>
+        <div className='contactForm'>
         <input type='text' placeholder='Name' />
         <input type='text' placeholder='Email' />
         <textarea placeholder='Message' />
-
+        <button className='buttons'>Send Message</button>
+        <button className='buttons'>Clear Form</button>
+        <a href='https://github.com/rnj98' target="_blank">
+          <button className='buttons'>Go to my GitHub profile</button>
+        </a>
       </div>
     </div>
   )
--- src/components/styles/Navbar.jsx
+++ src/components/styles/Navbar.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import logo from '../assets/logo-1000.png'
+import logo from '../assets/logo.png'
 import './Navbar.css'
 
 function Navbar() {
   return (
-    <div>
+    <div className='navContent'>
     <div className='logo-container'>
-        <img src={logo} style={{ width: '100px', padding: '5px'}}/>
+        <img src={logo} style={{ width: '170px', padding: '10px'}} alt="logo"/>
     </div>
     <nav className='nav'>
         <Link to="/">
-            <button>Home |</button>
+            <button>Home </button>
         </Link>
         <Link to="/projects">
-            <button>Projects |</button>
+            <button>Projects </button>
         </Link>
         <Link to="/contact">
             <button>Contact</button>
