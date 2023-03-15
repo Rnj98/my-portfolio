0292ef9cac79dc6726094f278728e0dbb72c0792
Ryan Jolley <ryan.jolley0@gmail.com>
2023-03-15 09:23:04 -0600

    changed root direct

--- src/components/pages/Contact/Contact.jsx
+++ src/components/pages/Contact/Contact.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import "./Contact.css"
+
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div className='mainContact'>
+      <div className='divGreeter'>I would love to hear from you!
+      <br>
+      </br>
+      If you have any questions, or would like a copy of my resume, please send me a message here!</div>
+      <br></br>
+      <div className='contactForm'>
+        <input type='text' placeholder='Name' />
+        <input type='text' placeholder='Email' />
+        <textarea placeholder='Message' />
+
+      </div>
+    </div>
   )
 }
