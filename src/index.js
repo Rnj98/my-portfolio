e1598b26debcbbe6e47aac4553b413e7374ec93a
Ryan Jolley <ryan.jolley0@gmail.com>
2023-03-14 22:09:19 -0600

    seeing what i changed

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter} from "react-router-dom";
 import './index.css'
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </HashRouter>
+  </BrowserRouter>
 );
